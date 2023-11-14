bca8854508e5d052db06a518e7e4c81eeacff503
texjunior <142810956+texjunior@users.noreply.github.com>
2023-11-13 23:50:35 -0300

    Update mainbox

--- mainboxG.C
+++ mainboxG.C
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_FILA 3
-#define MAX_PILHA 3
+#define MAX 3
 
 typedef struct
 {
@@ -13,13 +12,13 @@ typedef struct
 
 typedef struct
 {
-    Caixa *caixas[MAX_PILHA];
+    Caixa *caixas[MAX];
     int topo;
 } Pilha;
 
 typedef struct
 {
-    Caixa *caixas[MAX_FILA];
+    Caixa *caixas[MAX];
     int inicio;
     int fim;
 } Fila;
@@ -37,7 +36,7 @@ void inicializarFila(Fila *fila)
 
 int estaCheiaFila(Fila *fila)
 {
-    return fila->fim == MAX_FILA - 1;
+    return fila->fim == MAX - 1;
 }
 
 int estaVaziaFila(Fila *fila)
@@ -47,7 +46,7 @@ int estaVaziaFila(Fila *fila)
 
 int estaCheiaPilha(Pilha *pilha)
 {
-    return pilha->topo == MAX_PILHA - 1;
+    return pilha->topo == MAX - 1;
 }
 
 int estaVaziaPilha(Pilha *pilha)
@@ -97,7 +96,7 @@ void enfileirar(Fila *fila, Pilha *pilha1, Pilha *pilha2)
                 {
                     fila->inicio = 0;
                 }
-                fila->fim = (fila->fim + 1) % MAX_FILA;
+                fila->fim = (fila->fim + 1) % MAX;
 
                 fila->caixas[fila->fim] = novaCaixa;
 
@@ -111,12 +110,13 @@ void enfileirar(Fila *fila, Pilha *pilha1, Pilha *pilha2)
         {
             fila->inicio = 0;
         }
-        fila->fim = (fila->fim + 1) % MAX_FILA;
+        fila->fim = (fila->fim + 1) % MAX;
 
         fila->caixas[fila->fim] = novaCaixa;
 
         strcpy(novaCaixa->status, "AGUARDANDO");
         printf("Caixa de tamanho %d enfileirada.\n", novaCaixa->tamanho);
+        novaCaixa = NULL;
     }
 
     else
@@ -127,47 +127,73 @@ void enfileirar(Fila *fila, Pilha *pilha1, Pilha *pilha2)
 
 void empilhar(Fila *fila, Pilha *pilha1, Pilha *pilha2)
 {
-    for (int i = 0; i < MAX_FILA; i++)
+    if (pilha1->topo < MAX || pilha2->topo < MAX)
     {
-        Caixa *proximaCaixa = fila->caixas[i];
-        // comparar tamanho da caixa com o topo da pilha
-        if (!estaCheiaPilha(pilha1) && proximaCaixa->tamanho <= pilha1->caixas[pilha1->topo]->tamanho)
+
+        int i;
+        for (i = 0; i < MAX; i++)
         {
-            pilha1->topo++;
-            pilha1->caixas[pilha1->topo] = proximaCaixa;
-            strcpy(proximaCaixa->status, "EMPILHADA");
-            printf("Caixa de tamanho %d empilhada na Pilha 1.\n", proximaCaixa->tamanho);
-            free(fila->caixas[i]);
-            fila->caixas[i] = NULL;
-            fila->fim = fila->fim - 1;
+            Caixa *proximaCaixa = fila->caixas[i];
+
+            // Verificar se a pilha 1 esta cheia ou se a proxima caixa tem tamanho menor ou igual ao topo da pilha
+            if (pilha1->topo < 0)
+            {
+                pilha1->topo++;
+                pilha1->caixas[pilha1->topo] = proximaCaixa;
+                strcpy(proximaCaixa->status, "EMPILHADA");
+                printf("Caixa de tamanho %d empilhada na Pilha 1.\n", proximaCaixa->tamanho);
+                free(fila->caixas[i]);
+                fila->caixas[i] = NULL;
+                fila->fim = fila->fim - 1;
+            }
+            else if (pilha2->topo < 0)
+            {
+                pilha2->topo++;
+                pilha2->caixas[pilha2->topo] = proximaCaixa;
+                strcpy(proximaCaixa->status, "EMPILHADA");
+                printf("Caixa de tamanho %d empilhada na Pilha 2.\n", proximaCaixa->tamanho);
+                free(fila->caixas[i]);
+                fila->caixas[i] = NULL;
+                fila->fim = fila->fim - 1;
+            }
+            else if (!estaCheiaPilha(pilha1) && proximaCaixa->tamanho <= pilha1->caixas[pilha1->topo]->tamanho)
+            {
+                pilha1->topo++;
+                pilha1->caixas[pilha1->topo] = proximaCaixa;
+                strcpy(proximaCaixa->status, "EMPILHADA");
+                printf("Caixa de tamanho %d empilhada na Pilha 1.\n", proximaCaixa->tamanho);
+                free(fila->caixas[i]);
+                fila->caixas[i] = NULL;
+                fila->fim = fila->fim - 1;
+            }
+            else if (!estaCheiaPilha(pilha2) && proximaCaixa->tamanho <= pilha2->caixas[pilha2->topo]->tamanho)
+            {
+                pilha2->topo++;
+                pilha2->caixas[pilha2->topo] = proximaCaixa;
+                strcpy(proximaCaixa->status, "EMPILHADA");
+                printf("Caixa de tamanho %d empilhada na Pilha 2.\n", proximaCaixa->tamanho);
+                free(fila->caixas[i]);
+                fila->caixas[i] = NULL;
+                fila->fim = fila->fim - 1;
+            }
         }
-        else if (!estaCheiaPilha(pilha2) && proximaCaixa->tamanho <= pilha2->caixas[pilha2->topo]->tamanho)
-        {
-            pilha2->topo++;
-            pilha2->caixas[pilha2->topo] = proximaCaixa;
-            strcpy(proximaCaixa->status, "EMPILHADA");
-            printf("Caixa de tamanho %d empilhada na Pilha 2.\n", proximaCaixa->tamanho);
-            free(fila->caixas[i]);
-            fila->caixas[i] = NULL;
-            fila->fim = fila->fim - 1;
-        }
-        else
-        {
-            printf("nao foi possivel empilhar a caixa de tamanho %d\n", proximaCaixa->tamanho);
-            break;
-        }
+    }
+    else
+    {
+        printf("nao foi possivel empilhar outra caixa.\n");
     }
 }
 
 void desempilhar(Pilha *pilha)
 {
-    while(pilha->topo > -1 && pilha->caixas[pilha->topo] != NULL)
+    while (pilha->topo > -1 && pilha->caixas[pilha->topo] != NULL)
     {
         printf("Caixa de tamanho %d desempilhada.\n", pilha->caixas[pilha->topo]->tamanho);
         pilha->caixas[pilha->topo] = NULL;
+        free(pilha->caixas[pilha->topo]);
         pilha->topo--;
-    } 
-        printf("Pilha vazia. Nao e possivel desempilhar caixas.\n");
+    }
+    printf("Pilha vazia. Nao e possivel desempilhar caixas.\n");
 }
 
 void exibir(Fila *fila, Pilha *pilha1, Pilha *pilha2)
@@ -178,7 +204,9 @@ void exibir(Fila *fila, Pilha *pilha1, Pilha *pilha2)
 
     if (!estaVaziaFila(fila))
     {
-        for (int i = 0; i <= fila->fim; i++)
+
+        int i;
+        for (i = 0; i <= fila->fim; i++)
         {
             if (fila->caixas[i] != NULL)
             {
@@ -190,7 +218,8 @@ void exibir(Fila *fila, Pilha *pilha1, Pilha *pilha2)
 
     // Exibir pilha1
     printf(">>>>Pilha 1:\n");
-    for (int i = 0; i <= pilha1->topo; i++)
+    int i;
+    for (i = 0; i <= pilha1->topo; i++)
     {
         printf("caixa tamanho: %d %s\n", pilha1->caixas[i]->tamanho, pilha1->caixas[i]->status);
     }
@@ -198,7 +227,8 @@ void exibir(Fila *fila, Pilha *pilha1, Pilha *pilha2)
 
     // Exibir pilha2
     printf(">>>>Pilha 2:\n");
-    for (int i = 0; i <= pilha2->topo; i++)
+
+    for (i = 0; i <= pilha2->topo; i++)
     {
         printf("caixa tamanho: %d %s\n", pilha2->caixas[i]->tamanho, pilha2->caixas[i]->status);
     }
